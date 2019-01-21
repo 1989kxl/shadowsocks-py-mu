5e84dbfe7b482eb9bb79af360807d80e8c8bf560
1989kxl <32151714+1989kxl@users.noreply.github.com>
2019-01-22 00:45:50 +0800

    Update debian.sh

--- node/debian.sh
+++ node/debian.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-clear
-echo
-echo "#############################################################"
-echo "# 一键对接SSR后端modwebapi版本，支持Debian8+/Ubuntu16+      #"
-echo "# Author: Chikage <csp85123@gmail.com>                      #"
-echo "# Blog: www.94ish.me                                        #"
-echo "#############################################################"
-echo
-read -p "请输入此节点在面板中的ID号: " nodeid
-read -p "请输入完整面板域名/ip地址（例如https://www.94ish.me）: " host
-read -p "请输入modwebapi验证密钥: " pass
-read -p "请输入测速时间间隔（默认6）：" speedtest
-if [ -z "${speedtest}" ];then  
-    speedtest=6 
-fi
-echo "开始部署"
-sleep 2s
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-cd /root
-apt-get update
-apt-get -y install build-essential wget python-dev libffi-dev openssl python-pip libssl-dev zip unzip git
-wget https://github.com/1989kxl/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
-tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
-./configure && make -j2 && make install
-ldconfig
-cd .. && rm -f libsodium-1.0.10.tar.gz && rm -rf libsodium-1.0.10
-git clone -b manyuser https://github.com/1989kxl/shadowsocks.git
-cd shadowsocks
-pip install --upgrade setuptools
-pip install -r requirements.txt
-cp apiconfig.py userapiconfig.py
-cp config.json user-config.json
-chmod +x *.sh
+
+Shut_down_iptables(){
+	apt-get -y install iptables iptables-services
+	iptables -F;iptables -X
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save > /etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+}
+
+Setting_node_information(){
+	clear;echo "设定服务端信息:"
+	read -p "(1/3)前端地址:" Front_end_address
+		if [[ ${Front_end_address} = '' ]];then
+			Front_end_address=`curl -s "https://myip.ipip.net" | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}"`
+			echo "emm,我们已将前端地址设置为:http://${Front_end_address}"
+		fi
+	read -p "(2/3)节点ID:" Node_ID
+	read -p "(3/3)Mukey:" Mukey
+	if [[ ${Mukey} = '' ]];then
+		Mukey='mupass';echo "未设置该项,默认Mukey值为:mupass"
+	fi
+	echo;echo "Great！即将开始安装...";echo;sleep 2.5
+}
+
+install_node_for_debian(){
+	apt-get -y update;apt-get install -y wget curl git lsof python-pip build-essential
+	cd /root;wget "http://ssr-1252089354.coshk.myqcloud.com/libsodium-1.0.15.tar.gz"
+	tar xf /root/libsodium-1.0.15.tar.gz;cd /root/libsodium-1.0.15;./configure;make -j2;make install;cd /root
+	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf;ldconfig
 	
-	sed -i "17c WEBAPI_URL = \'${Front_end_address}\'" /../shadowsocks/userapiconfig.py
-	sed -i "2c NODE_ID = ${Node_ID}" /../shadowsocks/userapiconfig.py
-	sed -i "18c WEBAPI_TOKEN = \'${Mukey}\'" /../shadowsocks/userapiconfig.py
+	pip install cymysql requests -i https://pypi.org/simple/
+	wget -O /usr/bin/shadowsocks "https://raw.githubusercontent.com/qinghuas/ss-panel-and-ss-py-mu/master/node/ss";chmod 777 /usr/bin/shadowsocks
+	git clone -b manyuser https://github.com/glzjin/shadowsocks.git "/root/shadowsocks"
+	cd shadowsocks;chmod +x *.sh;pip install -r requirements.txt -i https://pypi.org/simple/
+	cp apiconfig.py userapiconfig.py;cp config.json user-config.json
+	
+	sed -i "17c WEBAPI_URL = \'${Front_end_address}\'" /root/shadowsocks/userapiconfig.py
+	sed -i "2c NODE_ID = ${Node_ID}" /root/shadowsocks/userapiconfig.py
+	sed -i "18c WEBAPI_TOKEN = \'${Mukey}\'" /root/shadowsocks/userapiconfig.py
+}
 
-
-
-
+Setting_node_information
+install_node_for_debian
+Shut_down_iptables
