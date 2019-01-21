064841f8189db44d399f1bee184b83a061131b1b
1989kxl <32151714+1989kxl@users.noreply.github.com>
2019-01-22 04:11:50 +0800

    Update debian.sh

--- node/debian.sh
+++ node/debian.sh
@@ -18,14 +18,14 @@ Setting_node_information(){
 }
 
 install_node_for_debian(){
-	apt-get -y update;apt-get -y install build-essential wget python-dev libffi-dev openssl python-pip libssl-dev zip unzip git
+	apt-get -y update;apt-get -y install build-essential wget python-dev libffi-dev python-pip libssl-dev zip unzip git curl lsof
 	cd /root;wget https://github.com/1989kxl/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz
 	tar xf /root/libsodium-1.0.17.tar.gz;cd /root/libsodium-1.0.17;./configure;make -j2;make install;cd /root
 	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf;ldconfig
-	
+	pip install cymysql requests -i https://pypi.org/simple/
 	wget -O /usr/bin/shadowsocks "https://raw.githubusercontent.com/1989kxl/shadowsocks-py-mu/master/node/ss";chmod 777 /usr/bin/shadowsocks
 	git clone -b manyuser https://github.com/1989kxl/shadowsocks.git "/root/shadowsocks"
-	cd shadowsocks;chmod +x *.sh;pip install --upgrade setuptools;pip install -r requirements.txt
+	cd shadowsocks;chmod +x *.sh;pip install --upgrade setuptools;pip install -r requirements.txt -i https://pypi.org/simple/
 	cp apiconfig.py userapiconfig.py;cp config.json user-config.json
 	
 	sed -i "17c WEBAPI_URL = \'${Front_end_address}\'" /root/shadowsocks/userapiconfig.py
