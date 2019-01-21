0a919f892b2922921ae49d9bfdefcb5f60531ecf
1989kxl <32151714+1989kxl@users.noreply.github.com>
2019-01-21 23:26:29 +0800

    Update debian.sh

--- node/debian.sh
+++ node/debian.sh
@@ -4,7 +4,7 @@ export PATH
 clear
 
 
-
+Setting_node_information(){
 	clear;echo "设定服务端信息:"
 	read -p "(1/3)前端地址:" Front_end_address
 		if [[ ${Front_end_address} = '' ]];then
@@ -17,8 +17,9 @@ clear
 		Mukey='mupass';echo "未设置该项,默认Mukey值为:mupass"
 	fi
 	echo;echo "Great！即将开始安装...";echo;sleep 2.5
+}
 
-
+install_node_for_debian(){
 	apt-get -y update
 	cd /root
 	apt-get -y install build-essential wget python-dev libffi-dev openssl python-pip libssl-dev zip unzip git
@@ -27,6 +28,7 @@ clear
         ./configure && make -j2 && make install
         ldconfig
         cd .. && rm -f libsodium-1.0.17.tar.gz && rm -rf libsodium-1.0.17
+	cd /root
         git clone -b manyuser https://github.com/1989kxl/shadowsocks.git
         cd shadowsocks
         pip install --upgrade setuptools
@@ -38,7 +40,10 @@ clear
 	sed -i "17c WEBAPI_URL = \'${Front_end_address}\'" /root/shadowsocks/userapiconfig.py
 	sed -i "2c NODE_ID = ${Node_ID}" /root/shadowsocks/userapiconfig.py
 	sed -i "18c WEBAPI_TOKEN = \'${Mukey}\'" /root/shadowsocks/userapiconfig.py
+}
 
+Setting_node_information
+install_node_for_debian
 
 
 
