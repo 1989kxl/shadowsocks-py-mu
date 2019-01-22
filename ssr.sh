a466c1bef2ac1508355e2f54d4c18c7e0ceded07
1989kxl <32151714+1989kxl@users.noreply.github.com>
2019-01-23 00:31:15 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -266,7 +266,9 @@ Install_supervisor(){
 		     mv -f /root/tools/supervisord.conf /etc/supervisor
 		     supervisor_conf_modify_debian
 		     /etc/init.d/supervisor restart
+		     sleep 4
                      supervisorctl reload
+		     sleep 2
                      supervisorctl restart ssr
                      sleep 2.5
                      echo -e "${OK} ${GreenBG} supervisor 安装成功 ${Font}"
