d3878ec567a9b570e801ca1cc00b547ffae99b4e
1989kxl <32151714+1989kxl@users.noreply.github.com>
2019-03-31 14:23:10 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -168,30 +168,32 @@ Install_ss_node(){
 	read -p "是否安装swap交换分区？(Y/n)" choice
 	case "${choice}" in
 	y|Y) Add_swap_partition;;
-	n|N) exit;;
-	*) exit;;
-	
+	n|N) echo "跳过.";echo;;
+	*) echo "跳过.";echo;;
+	esac
 	sleep 2.5
 	
 	read -p "是否安装fail2ban防火墙？(Y/n)" choice
 	case "${choice}" in
 	y|Y) Install_fail2ban;;
-	n|N) exit;;
-	*) exit;;
-	
+	n|N) echo "跳过.";echo;;
+	*) echo "跳过.";echo;;
+	esac
 	sleep 2.5
 	
 	read -p "是否安装supervisor守护？(Y/n)" choice
         case "${choice}" in
 	y|Y) Install_supervisor;;
-	n|N) exit;;
-	*) exit;;
-	
+	n|N) echo "跳过.";echo;;
+	*) echo "跳过.";echo;;
+	esac
 	sleep 2.5 
+	
+	
 	#Installation_end_time=`date +"%Y-%m-%d %H:%M:%S"`;Install_end_time_stamp=`date +%s`
 	#The_installation_time=`expr ${Install_end_time_stamp} - ${Install_the_start_time_stamp}`
 	#clear;echo "安装开始时间:[${Setup_time}],安装结束时间:[${Installation_end_time}],耗时[${The_installation_time}]s."
-	echo "安装已完成.但ssr服务尚未启动,请通过[shadowsocks]命令管理服务."
+	echo "安装已完成"
 }
 
 Edit_ss_node_info(){
